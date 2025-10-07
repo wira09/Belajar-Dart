221668cf56e56186d7e6c559c1503cb32ad71614
Wira <zmuhamad064@gmail.com>
2025-10-07 19:58:21 +0700

    07.Dart_Type_Systems & 08.Dart_Futures

---
+++ 08.Dart_Futures/01.Apa itu Future/latih2.dart
@@ -0,0 +1,13 @@
+// Completed with data
+void main() {
+  getOrder().then((value) {
+    print('Your ordered: $value');
+  });
+  print('Getting your order...');
+}
+
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    return 'Coffee Boba';
+  });
+}
